a02a7491db0ed579d8c4c40cf33b1e4139adc777
Dhikshith Reddy <dhikshithreddy12@gmail.com>
2025-02-06 01:10:20 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/persistent_scroll/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.persistent_scroll
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/list_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ListWidget extends StatelessWidget {
+  const ListWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    var list = <Widget>[];
+    for (var i = 0; i <= 100; i++) {
+      list.add(const ListItemWidget());
+    }
+    return ListView.builder(itemCount: list.length,itemBuilder: (context,index)=> list[index]);
+  }
+}
+
+class ListItemWidget extends StatefulWidget {
+  const ListItemWidget({super.key});
+  @override
+  State<ListItemWidget> createState() => _ListItemWidgetState();
+}
+
+
+class _ListItemWidgetState extends State<ListItemWidget> with AutomaticKeepAliveClientMixin{
+  var count = 0;
+  @override
+  bool get wantKeepAlive => true;
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Container(
+        padding: const EdgeInsets.all(4),
+        child: Row(
+          children: [
+            Text(count.toString()),
+            MaterialButton(
+              onPressed: () {
+                setState(() {
+                  count++;
+                });
+              },
+              child: const Text("+"),
+            )
+          ],
+        ));
+  }
+}
